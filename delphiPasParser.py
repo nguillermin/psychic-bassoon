98835b5fd598391f0a5ef932b93882d37a8a4c4f
nils <nils@odin-inc.com>
2016-11-14 14:57:02 -0500

    Bad commit msg, sry

--- delphiPasParser.py
+++ delphiPasParser.py
@@ -1,41 +1,51 @@
-from delphiPasGrammar import grammar
+import delphiPasGrammar
 
-PasParser = grammar
+PasParser = delphiPasGrammar.grammar
 
 ###############################################################################
 ### Classes
 ###############################################################################
 
-class Node(list):
+
+class Node():
     """
     AST node
     """
-    def __repr__(self):
-        return '%s(%s)' % (self.__class__.__name__, list.__repr__(self))
+    def __eq__(self, other):
+        return self.__class__ == other.__class__ # And contents?
+
+
+    # def __repr__(self):
+    #     return '%s(%s)' % (self.__class__.__name__, Node.__repr__(self))
+
 
     @classmethod
     def group(cls, expr):
         def group_action(s, loc, tok):
             try:
-                lst = t[0].asList()
+                lst = tok[0].asList()
             except (IndexError, AttributeError) as e:
-                lst = t
-            return [cls(lst)]
+                lst = tok
+            return Node(lst)
 
         return Group(expr).setParseAction(group_action)
 
 
 def main(argv):
-    with open(argv, 'r') as f:
-        string = f.read()
-    grammar.parseString(string).pprint()
+    grammarTypes = [item for item in dir(delphiPasGrammar) if item[0] not in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' and item[0:2]!='__']
+    # print(grammarTypes)
+    art = [type()]
+
+
+    # with open(argv, 'r') as f:
+    #     string = f.read()
+    # grammar.parseString(string).pprint()
 
 
 if __name__ == "__main__":
     import pprint, sys
 
     if len(sys.argv)==1:
-        # run tests
-        pass
+        main('shim')
     else:
         main(sys.argv[1])
--- pruneOrphanComponents.py
+++ pruneOrphanComponents.py
@@ -1,21 +1,47 @@
 from delphiPasParser import titleAndInterface # Until the parser is fully functional, just the parts that work
 from delphiDfmParser import DfmParser
 
+import os, sys
+import chardet
+
 def main(arg):
     filename = os.path.splitext(os.path.split(arg)[1])[0]
 
-    with open('../Delphi/'+filename+'.dfm', 'r', encoding='ascii') as f:
+    dfmFile = '../Delphi/'+filename+'.dfm'
+    pasFile = '../Delphi/'+filename+'.pas'
+
+    f = open(dfmFile,'rb')
+    guess = chardet.detect(f.read(1000))
+    print(guess)
+    if guess['confidence']==1.0:
+        codec = guess['encoding']
+    else:
+        print('Not sure on '+filename+'.dfm encoding, exiting.')
+        exit()
+    f.close()
+
+    with open(dfmFile, 'rb') as f:
         string = f.read()
-    parseResults = DfmParser.parseString(string)
+    parseResults = DfmParser.parseString(string.decode('utf-8'))
     dfmComponents = [n[0][0] for n in parseResults[2:]]
     
-    with open('../Delphi/'+filename+'.pas', 'r', encoding='utf8') as f:
+    f = open(pasFile,'rb')
+    guess = chardet.detect(f.read(1000))
+    print(guess)
+    if guess['confidence']==1.0:
+        codec = guess['encoding']
+    else:
+        print('Not sure on '+filename+'.dfm encoding, exiting.')
+        exit()
+    f.close()
+
+    with open(pasFile, 'rb') as f:
         string = f.read()
-    parseResults = titleAndInterface.parseString(string).pprint()
+    parseResults = titleAndInterface.parseString(string.decode('utf-8'))
+    print(parseResults.dump())
 
 
 if __name__ == "__main__":
-    import pprint, os, sys
     if len(sys.argv)==1:
         raise ValueError("No arguments specified")
         exit()
