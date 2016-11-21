d5ba84fb424b047243678652c49506aa2dc64936
Nils Guillermin <nils.guillermin@gmail.com>
2016-11-20 21:47:34 -0500

    grammar types are now class:Node

--- delphiPasParser.py
+++ delphiPasParser.py
@@ -7,7 +7,7 @@ PasParser = delphiPasGrammar.grammar
 ###############################################################################
 
 
-class Node():
+class Node(list):
     """
     AST node
     """
@@ -15,8 +15,8 @@ class Node():
         return self.__class__ == other.__class__ # And contents?
 
 
-    # def __repr__(self):
-    #     return '%s(%s)' % (self.__class__.__name__, Node.__repr__(self))
+    def __repr__(self):
+        return '%s(%s)' % (self.__class__.__name__, Node.__repr__(self))
 
 
     @classmethod
@@ -31,21 +31,36 @@ class Node():
         return Group(expr).setParseAction(group_action)
 
 
+def makeNode(_typeName):
+    node = type(str(_typeName)+"Node", (Node,), {})
+
+
 def main(argv):
-    grammarTypes = [item for item in dir(delphiPasGrammar) if item[0] not in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' and item[0:2]!='__']
-    # print(grammarTypes)
-    art = [type()]
+    grammarTypes = [item for item in dir(delphiPasGrammar) if 
+            item[0] not in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' and item[0:2]!='__']
 
+    art = {x: type(str(x)+"Node", (Node,), {}) for x in grammarTypes}
 
-    # with open(argv, 'r') as f:
-    #     string = f.read()
-    # grammar.parseString(string).pprint()
+    for key in art.keys():
+        globals()[key] = art[key].group(globals()[key])
+
+    if argv=='':
+        exit()
+    else:
+        with open(argv[1], 'rb') as f:
+            detect = chardet.detect(f.read())
+
+        print('Encoding used: ' + detect['encoding'] + ' with confidence: ' + repr(detect['confidence']))
+        with open(argv[1],'r', encoding=detect['encoding']) as f:
+            string = f.read()
+        grammar.parseString(string).pprint()
 
 
 if __name__ == "__main__":
-    import pprint, sys
+    from delphiPasGrammar import *
+    import pprint, sys, chardet
 
     if len(sys.argv)==1:
-        main('shim')
+        main('')
     else:
-        main(sys.argv[1])
+        main(sys.argv)
